e75198647b841120ae85b6e43e3c833eb3d6e545
nislederz <nislederz@gmail.com>
2021-10-19 13:24:28 -0500

    cambios

--- Vehicles.API/Startup.cs
+++ Vehicles.API/Startup.cs
@@ -71,7 +71,7 @@ namespace Vehicles.API
             services.AddTransient<SeedDb>();
             services.AddScoped<IUserHelper, UserHelper>();
             services.AddScoped<ICombosHelper, CombosHelper>();
-            //services.AddScoped<IBlobHelper, BlobHelper>();
+            services.AddScoped<IBlobHelper, BlopHelper>();
             services.AddScoped<IConverterHelper, ConverterHelper>();
             services.AddScoped<IMailHelper, MailHelper>();
             services.AddAzureClients(builder =>
